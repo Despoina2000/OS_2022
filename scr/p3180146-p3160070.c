c08ed5b8c7878f58e19b419ae9b4e0122969ba37
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2022-05-12 19:53:39 +0300

    Adding files

---
+++ scr/p3180146-p3160070.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void * execute(void * customer_id){}
+
+int main(int argc, char *argv[]){}
