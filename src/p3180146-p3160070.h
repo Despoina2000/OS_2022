0e5b8e6950e7fc444534badbe2154ca6d342b231
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2022-05-30 14:58:29 +0300

    adding double list

--- src/p3180146-p3160070.h
+++ src/p3180146-p3160070.h
@@ -23,7 +23,7 @@
 #define tcashhigh 8
 #define Pcardsucces 0.9
 
-* Node of a doubly linked list */
+/* Node of a doubly linked list */
 struct Node {
     int data;
     struct Node* next; // Pointer to next node in DLL
