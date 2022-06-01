dc4976a8f2bdaa62a5208316cf75f7a36639d1d4
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2022-06-01 20:25:21 +0300

    new files

---
+++ src/p3180146-p3160070_-_Copy.c
@@ -0,0 +1,273 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Mutexes
+pthread_mutex_t mutex_telephonist= PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_cashier = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_seatA = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_seatB = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_listA = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_listB = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_printer = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_balance= PTHREAD_MUTEX_INITIALIZER;
+
+//Condition
+pthread_cond_t cond_telephonist = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_cashier = PTHREAD_COND_INITIALIZER;
+
+//Variable
+int *zoneA[ NzoneA, Nseat];
+int *zoneB[ NzoneB, Nseat];
+struct Node* listA[ NzoneA];
+struct Node* listB[ NzoneB];
+int telephonist;
+int cashier;
+unsigned int seed;
+double balance;
+double sum_waiting_time;
+double sum_service_time;
+int sum_typeA;// κράτηση ολοκληρώθηκε επιτυχώς
+int sum_typeB;// κράτηση απέτυχε γιατί δεν υπάρχουν κατάλληλες θέσεις
+int sum_typeC;// κράτηση απέτυχε γιατί η συναλλαγή με την πιστωτική κάρτα δεν έγινε αποδεκτή.
+
+void * processing(void * customer_id){
+
+
+    struct timespec timer_on;
+	struct timespec	service_timer;
+	struct timespec timer_off;
+    int id=*(int *)customer_id;
+
+    clock_gettime(CLOCK_REALTIME, &timer_on); //Start timer
+    pthread_mutex_lock(&mutex_telephonist);
+    while(telephonist <= 0)
+	{
+	pthread_cond_wait(&cond_telephonist ,&mutex_telephonist);
+	}
+	--telephonist;
+	clock_gettime(CLOCK_REALTIME, &service_timer);
+    pthread_mutex_unlock(&mutex_telephonist);
+
+    int choosen_seats = rand_r(&seed) % Nseathigh + Nseatlow;
+	int respond_time = rand_r(&seed) % tseathigh + tseatlow;
+    bool possible_zone= rand_r(&seed) % 100 / 100.0f <= PzoneB;
+	sleep(respond_time);
+
+    ;
+    bool flag_success_finding_seats=0;
+	//bool flag_success_payment=0;
+    int line=-1;
+    if(possible_zone){//searching zone A
+	   int i=0; 
+	   pthread_mutex_lock(&mutex_seatA) 
+		   while(i<NzoneA && !flag_success_finding_seats){
+		   struct Node list= listA[i];
+		while (list != NULL) {
+			if((list->next->data-list->data)+1=>choosen_seats){//βρήκαμε πιθανές θέσεις για τον πελάτη
+				flag_success_finding_seats=1;
+				pthread_mutex_lock(&mutex_cashier);
+				while(cashier <= 0)
+				{
+				pthread_cond_wait(&cond_cashier ,&mutex_cashier);
+				}
+				cashier--;
+					if(rand_r(&seed) % 100 / 100.0f <= Pcardsucces){//με επιτυχία πλήρωσε: Type A
+						//flag_success_payment=1;
+						sum_typeA++;
+						
+						pthread_mutex_lock(&mutex_balance);
+						balance += choosen_seats * CzoneA;
+						pthread_mutex_unlock(&mutex_balance);
+					//book θέσεις
+						j=list->data;
+						sum=0;
+						while(j<=list->next->data && sum<choosen_seats ){
+							zoneA[i,j]=customer_id;
+							j++;
+							sum++;
+						}
+						//update list with j 
+						if((list->next->data-list->data)+1=choosen_seats) deleteNode(&listA[i], j-1);
+						deleteNode(&listA[i], list->data);
+						push(&listA[i], j);
+						sortList(&listA[i]);
+					}
+					
+				
+				else{//βρήκε θέσεις αλλά δεν μπόρεσε να πληρώσει: Type C
+					sum_typeC++;
+				}pthread_mutex_unlock(&mutex_cashier);
+			}
+			list = list->next->next;
+			if(flag_success_finding_seats) break;
+
+    	}
+		pthread_mutex_unlock(&mutex_seatA);
+		if(!flag_success_finding_seats) sum_typeB++;
+		++i;
+	 	}
+		 
+	   }
+
+else{//searching zone B
+	pthread_mutex_lock(&mutex_seatB);
+    int i=0;  
+		   while(i<NzoneB && !flag_success_finding_seats){
+		   struct Node list=listB[i];
+		while (list != NULL) {
+			if((list->next->data-list->data)+1=>choosen_seats){//βρήκαμε πιθανές θέσεις για τον πελάτη
+				flag_success_finding_seats=1;
+				pthread_mutex_lock(&mutex_cashier);
+				while(cashier <= 0)
+				{
+				pthread_cond_wait(&cond_cashier ,&mutex_cashier);
+				}
+				cashier--;
+					if(rand_r(&seed) % 100 / 100.0f <= Pcardsucces){//με επιτυχία πλήρωσε: Type A
+						//flag_success_payment=1;
+						
+						sum_typeA++;
+						pthread_mutex_lock(&mutex_balance);
+						balance += choosen_seats * CzoneB;
+						pthread_mutex_unlock(&mutex_balance);
+						//book θέσεις
+						j=list->data;
+						sum=0;
+						while(j<=list->next->data && sum<choosen_seats ){
+							zoneB[i,j]=customer_id;
+							j++;
+							sum++;
+						}
+						//update list with j 
+						if((list->next->data-list->data)+1=choosen_seats) deleteNode(&listB[i], j-1);
+						deleteNode(&listB[i], list->data);
+						push(&listB[i], j);
+						sortList(&listB[i]);
+					}
+				
+				else{//βρήκε θέσεις αλλά δεν μπόρεσε να πληρώσει: Type C
+					sum_typeC++;
+				}
+				pthread_mutex_unlock(&mutex_cashier);
+			}
+			list = list->next->next;
+			if(flag_success_finding_seats) break;
+
+    	}
+		if(!flag_success_finding_seats) sum_typeB++;
+		++i;
+	 	}
+		 
+	   }
+	   pthread_mutex_lock(&mutex_seatB);
+  
+} 
+    
+    
+    
+    
+
+int main(int argc, char *argv[]){
+	if (argc != 3) {
+		printf("ERROR: Το προγραμμα πρεπει να παιρνει 2 arguments(number of clients--> int, seed --> int)\n\n\n");
+		exit(-1);
+	}
+	
+	int customers = atoi(argv[1]);
+   	if ( customers <= 0) {
+		printf("ERROR: Ο αριθμός των πελατών πρεπει να ειναι ακεραιος θετικός\n");
+		exit(-1);
+	}
+
+	balance = 0;
+    sum_waiting_time = 0;
+    sum_service_time = 0;
+	telephonist = Ntel;
+	seed = atoi(argv[2]);
+	printf("\n \nStart simulation with %d customers \n \n \n", customers);
+	
+
+	//Initializing seats
+	for(int i=0; i<NzoneA; ++i){
+		push(*listA[i],0);
+		push(*listA[i],9);
+		for(int j=0;j<Nseat;++j)
+		{
+			zoneA[i,j] = -1;
+		}
+	}
+
+	for(int i=0; i<NzoneB; ++i){
+		push(*listB[i],0);
+		push(*listB[i],9);
+		for(int j=0;j<Nseat;++j)
+		{
+			zoneB[i,j] = -1;
+		}
+	}
+	
+
+	
+	
+
+	/*Create threads*/
+	pthread_t id[customers];
+	for(int i=0; i<customers; ++i)
+	{
+		pthread_create(&id[i], NULL, &execute, &i);
+	}
+
+
+
+	/*Wait all threads to finish*/
+	for(int i=0; i<customers; ++i)
+	{
+		pthread_join(id[i], NULL);
+	}
+
+	/*Print results*/
+	
+	//if a seat is not booked the result will be -1
+
+
+	printf("Πλάνο θέσεων: \n \n");
+	for(int i=0; i<NzoneA; ++i){
+		for (int j = 0; j < Nseat; j++) 
+		{	if(zoneA[i,j] != -1)
+			printf("Ζώνη Α / Σειρά %d / Θέση %d / Πελάτης %d\n", i, j, zoneA[i,j]);
+			else
+			printf("Ζώνη Α / Σειρά %d / Θέση %d / κανένας \n", i, j);
+		}
+	}
+
+	for(int i=0; i<NzoneB; ++i){
+		for (int j = 0; j < Nseat; j++) 
+		{	if(zoneB[i,j] != -1)
+			printf("Ζώνη B / Σειρά %d / Θέση %d / Πελάτης %d\n", i, j, zoneB[i,j]);
+			else
+			printf("Ζώνη B / Σειρά %d / Θέση %d / κανένας \n", i, j);
+		}
+	}
+
+	printf("\n");
+	printf("Τα συνολικά έσοδα είναι: %f € \n", balance);
+
+	printf("Το ποσοστό των συναλλαγών όπου η κράτηση ολοκληρώθηκε επιτυχώς: %f \n", sum_typeA/customers);
+
+	printf("Το ποσοστό των συναλλαγών όπου η κράτηση απέτυχε γιατί δεν υπάρχουν κατάλληλες θέσεις: %f \n", sum_typeB/customers);
+
+	printf("Το ποσοστό των συναλλαγών όπου η κράτηση απέτυχε γιατί η συναλλαγή με την πιστωτική κάρτα δεν έγινε αποδεκτή: %f \n", sum_typeC/customers);
+	
+	printf("Ο μέσος χρόνος αναμονής είναι: %f \n", sum_waiting_time/customers);
+	
+	printf("Ο μέσος χρόνος εξυπηρέτησης είναι: %f \n", sum_service_time /customers);
+
+
+
+
+	
+	return 0;   
+}
---
+++ src/p3180146-p3160070_-_Copy.h
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define Ntel 3
+#define Ncash 2
+#define Nseat 10
+#define NzoneA 10
+#define NzoneB 20
+#define PzoneB 0.7
+#define CzoneA 30
+#define CzoneB 20
+#define Nseatlow 1 // Number of tickets
+#define Nseathigh 5
+#define treslow 1 // Next customer call
+#define treshigh 5
+#define tseatlow 5 // Seconds operator takes to book
+#define tseathigh 13
+#define tcashlow 4 // Seconds operator takes to bill card
+#define tcashhigh 8
+#define Pcardsucces 0.9 // Card approval success rate
+
+/* Node of a doubly linked list */
+struct Node {
+    int data;
+    struct Node* next; // Pointer to next node in DLL
+    struct Node* prev; // Pointer to previous node in DLL
+};
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+ 
+    /* 2. put in the data  */
+    new_node->data = new_data;
+ 
+    /* 3. Make next of new node as head and previous as NULL */
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+ 
+    /* 4. change prev of head node to new node */
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+ 
+    /* 5. move the head to point to the new node */
+    (*head_ref) = new_node;
+}
+
+/* Given a node as prev_node, insert a new node after the given node */
+void insertAfter(struct Node* prev_node,  int new_data)
+{
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL) {
+        printf("the given previous node cannot be NULL");
+        return;
+    }
+ 
+    /* 2. allocate new node */
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+ 
+    /* 3. put in the data  */
+    new_node->data = new_data;
+ 
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;
+ 
+    /* 5. Make the next of prev_node as new_node */
+    prev_node->next = new_node;
+ 
+    /* 6. Make prev_node as previous of new_node */
+    new_node->prev = prev_node;
+ 
+    /* 7. Change previous of new_node's next node */
+    if (new_node->next != NULL)
+        new_node->next->prev = new_node;
+}
+
+void deleteNode(struct Node** head_ref, int key){
+    // Store head node
+    struct Node *temp = *head_ref;
+ 
+    // If head node itself holds the key to be deleted
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // Changed head
+        free(temp); // free old head
+        return;
+    }
+ 
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+ 
+    // Unlink the node from linked list
+    prev->next = temp->next;
+ 
+    free(temp); // Free memory
+}
+
+void sortList(struct Node** head_ref) {  
+    struct Node *current = NULL, *index = NULL;  
+    int temp;  
+    //Check whether list is empty  
+    if(head_ref == NULL) {  
+        return;  
+    }  
+    else {  
+        //Current will point to head  
+        for(current = &head_ref; current->next != NULL; current = current->next) {  
+            //Index will point to node next to current  
+            for(index = current->next; index != NULL; index = index->next) {  
+                //If current's data is greater than index's data, swap the data of current and index  
+                if(current->data > index->data) {  
+                    temp = current->data;  
+                    current->data = index->data;  
+                    index->data = temp;  
+                }  
+            }  
+        }  
+    }  
+}  
+
+/* Given a reference (pointer to pointer) to the head
+   of a DLL and an int, appends a new node at the end  */
+void append(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+ 
+    struct Node* last = *head_ref; /* used in step 5*/
+ 
+    /* 2. put in the data  */
+    new_node->data = new_data;
+ 
+    /* 3. This new node is going to be the last node, so
+          make next of it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new
+          node as head */
+    if (*head_ref == NULL) {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+ 
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+ 
+    /* 6. Change the next of last node */
+    last->next = new_node;
+ 
+    /* 7. Make last node as previous of new node */
+    new_node->prev = last;
+ 
+    return;
+}
+
+// This function prints contents of linked list starting
+// from the given node
+void printList(struct Node* node)
+{
+    struct Node* last;
+    printf("\nTraversal in forward direction \n");
+    while (node != NULL) {
+        printf(" %d ", node->data);
+        last = node;
+        node = node->next;
+    }
+ 
+    printf("\nTraversal in reverse direction \n");
+    while (last != NULL) {
+        printf(" %d ", last->data);
+        last = last->prev;
+    }
+}
+
+
+
+
